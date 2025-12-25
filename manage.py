46ab1e32df62e5ada2576d33b92c60ed73a1ee66
PoreVijaya <97454933+PoreVijaya@users.noreply.github.com>
2025-12-25 18:28:35 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_master.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
