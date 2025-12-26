f170a36b52f6dab81c444879536d61841595a8ad
PoreVijaya <97454933+PoreVijaya@users.noreply.github.com>
2025-12-26 11:32:29 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,10 +7,11 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "study_master.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError:
         raise
     execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
     main()
+
