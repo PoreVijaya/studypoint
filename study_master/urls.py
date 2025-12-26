9711137de9920d6bc9bc9b1134309d6720170e9e
PoreVijaya <97454933+PoreVijaya@users.noreply.github.com>
2025-12-26 13:09:41 +0530

    Update urls.py

--- study_master/urls.py
+++ study_master/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from school import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
