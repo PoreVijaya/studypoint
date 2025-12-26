9fb38036b71ebb8942eb12953a5430c9814eb2e6
PoreVijaya <97454933+PoreVijaya@users.noreply.github.com>
2025-12-26 12:01:16 +0530

    Update settings.py

--- study_master/settings.py
+++ study_master/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 # ==================================================
 # URLS / WSGI
 # ==================================================
-ROOT_URLCONF = "project.urls"
-WSGI_APPLICATION = "project.wsgi.application"
+ROOT_URLCONF = "study_master.urls"
+WSGI_APPLICATION = "study_master.wsgi.application"
 
 # ==================================================
 # TEMPLATES
