13e2ca796ddd11c408b9b90b29990fec26244f07
PoreVijaya <97454933+PoreVijaya@users.noreply.github.com>
2025-12-26 10:36:29 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,17 +2,15 @@
 import os
 import sys
 
-# ✅ FORCE PyMySQL BEFORE DJANGO LOADS
-import pymysql
-pymysql.install_as_MySQLdb()
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_master.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "study_master.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise
     execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
     main()
